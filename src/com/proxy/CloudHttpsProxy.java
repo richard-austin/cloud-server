191dcb26a453138eeef396d2ab332cc545d464f6
austinr <richard.david.austin@gmail.com>
2021-12-20 17:38:17 +0000

    Working for requests and streams.

--- src/com/proxy/CloudHttpsProxy.java
+++ src/com/proxy/CloudHttpsProxy.java
@@ -1,13 +1,13 @@
 package com.proxy;
 
 
-import com.sun.net.httpserver.*;
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 
 import javax.net.ssl.*;
 import java.io.*;
-import java.net.InetSocketAddress;
-;
 
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.util.*;
@@ -20,7 +20,7 @@ import static com.proxy.SslUtil.*;
 public class CloudHttpsProxy implements SslContextProvider {
     public static void main(String[] args) throws IOException {
         try {
-            new CloudHttpsProxy().authenticate("192.168.0.29", 443);
+            new CloudHttpsProxy().runServer("192.168.0.29", 443, 8082);
         } catch (Exception e) {
             System.err.println(e); //Prints the standard errors
         }
@@ -30,24 +30,11 @@ public class CloudHttpsProxy implements SslContextProvider {
     private final int threadPoolSize = 20;
     private final ExecutorService threadPoolExecutor = Executors.newFixedThreadPool(threadPoolSize);
 
-    static String JSESSIONID;
+    static String JSESSIONID="";
     static String baseUrl;
 
-    void runServer() {
+    String authenticate(SSLSocket socket) {
         try {
-            HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
-          //  server.createContext("/test", new HttpRequestHandler());
-            server.setExecutor(threadPoolExecutor);
-            server.start();
-            logger.info(" Server started on port 8001");
-        } catch (Exception ex) {
-            System.out.println("Exception in runServer: " + ex.getMessage() + "\n" + Arrays.toString(ex.getStackTrace()));
-        }
-    }
-
-    String authenticate(String host, int port) {
-        try {
-            SSLSocket socket = createSSLSocket(host, port);
             OutputStream os = socket.getOutputStream();
             InputStream is = socket.getInputStream();
             byte[] buf = new byte[500];
@@ -94,6 +81,128 @@ public class CloudHttpsProxy implements SslContextProvider {
         }
 
         return JSESSIONID;
+    }
+
+    /**
+     * It will run a single-threaded proxy server on
+     * the provided local port.
+     */
+    public void runServer(String host, int remoteport, int localport)
+            throws IOException {
+        // Creating a ServerSocket to listen for connections with
+        ServerSocket s = new ServerSocket(localport);
+        while (true) {
+            Socket client = null;
+
+            try {
+                // It will wait for a connection on the local port
+                client = s.accept();
+
+
+                requestProcessing(client, host, remoteport);
+            } catch (Exception ex) {
+
+            }
+        }
+    }
+
+    void requestProcessing(@NotNull Socket client, String host, int remoteport)
+    {
+        threadPoolExecutor.execute(() -> handleClientRequest(client, host, remoteport));
+    }
+
+    private void handleClientRequest(@NotNull Socket client, String host, int remoteport) {
+        SSLSocket server = null;
+        try {
+            final byte[] request = new byte[1024];
+            byte[] reply = new byte[4096];
+
+            final InputStream streamFromClient = client.getInputStream();
+            final OutputStream streamToClient = client.getOutputStream();
+
+            // Create a connection to the real server.
+            // If we cannot connect to the server, send an error to the
+            // client, disconnect, and continue waiting for connections.
+            try {
+                server =  createSSLSocket(host, remoteport);
+                if(Objects.equals(JSESSIONID, ""))
+                    authenticate(server);
+            } catch (IOException e) {
+                PrintWriter out = new PrintWriter(streamToClient);
+                out.print("Proxy server cannot connect to " + host + ":"
+                        + remoteport + ":\n" + e + "\n");
+                out.flush();
+                client.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // Get server streams.
+            assert server != null;
+            final InputStream streamFromServer = server.getInputStream();
+            final OutputStream streamToServer = server.getOutputStream();
+
+            // a thread to read the client's requests and pass them
+            // to the server. A separate thread for asynchronous.
+            final SSLSocket finalServer = server;
+            threadPoolExecutor.execute(() -> {
+                int bytesRead;
+                try {
+                    boolean firstRead = true;
+                    while ((bytesRead = streamFromClient.read(request)) != -1) {
+                        if(firstRead) {
+                            firstRead = false;
+                            HttpMessage msg = new HttpMessage(request);
+                            msg.buildHeaders();
+                            List<String> js = new ArrayList<String>();
+                            js.add("JSESSIONID="+JSESSIONID);
+                            msg.put("Cookie", js);
+                            String headers = msg.getHeaders();
+                            String messageBody = new String(msg.getMessageBody());
+                            streamToServer.write(headers.getBytes(StandardCharsets.UTF_8));
+                            streamToServer.write(msg.getMessageBody(), 0, bytesRead-headers.length()+1);
+                            streamToServer.flush();
+                        }
+                        else {
+                            streamToServer.write(request, 0, bytesRead);
+                            streamToServer.flush();
+                        }
+                    }
+                } catch (IOException e) {
+                }
+
+                // the client closed the connection to us, so close our
+                // connection to the server.
+                try {
+                    streamToServer.close();
+                } catch (IOException e) {
+                }
+            });
+
+            // Read the server's responses
+            // and pass them back to the client.
+            int bytesRead;
+            try {
+                while ((bytesRead = streamFromServer.read(reply)) != -1) {
+                    streamToClient.write(reply, 0, bytesRead);
+                    streamToClient.flush();
+                }
+            } catch (IOException e) {
+            }
+            // The server closed its connection to us, so we close our
+            // connection to our client.
+            streamToClient.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                if (server != null)
+                    server.close();
+                if (client != null)
+                    client.close();
+            } catch (IOException e) {
+            }
+        }
     }
 
     private SSLSocket createSSLSocket(String host, int port) throws Exception {
