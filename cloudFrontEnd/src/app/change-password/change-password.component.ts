00bf830c322d9462a8a5ed158f737edab5e2415b
austinr <richard.david.austin@gmail.com>
2022-02-21 21:22:38 +0000

    Use passwordValidator directly instead of returning the function from an accessor function.

--- cloudFrontEnd/src/app/change-password/change-password.component.ts
+++ cloudFrontEnd/src/app/change-password/change-password.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 import {ChangePasswordService} from "./change-password.service";
 import {ReportingComponent} from "../reporting/reporting.component";
 import {HttpErrorResponse} from "@angular/common/http";
@@ -9,17 +9,18 @@ import {HttpErrorResponse} from "@angular/common/http";
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.scss']
 })
-export class ChangePasswordComponent implements OnInit{
+export class ChangePasswordComponent implements OnInit {
 
   changePasswordForm!: FormGroup;
   oldPassword: string = '';
   newPassword: string = '';
   confirmNewPassword: string = '';
-  @ViewChild(ReportingComponent) reporting!:ReportingComponent;
+  @ViewChild(ReportingComponent) reporting!: ReportingComponent;
 
-  constructor(private changePasswordService:ChangePasswordService) { }
+  constructor(private changePasswordService: ChangePasswordService) {
+  }
 
-  hasError = (controlName: string, errorName: string):boolean | undefined =>{
+  hasError = (controlName: string, errorName: string): boolean | undefined => {
     return this.changePasswordForm.controls[controlName].hasError(errorName);
   }
 
@@ -30,43 +31,38 @@ export class ChangePasswordComponent implements OnInit{
   formSubmitted() {
     this.oldPassword = this.getFormControl('oldPassword').value;
     this.changePasswordService.changePassword(this.oldPassword, this.newPassword, this.confirmNewPassword).subscribe(() => {
-      this.reporting.successMessage="Password changed";
-    },
-    (reason: HttpErrorResponse) => {
-      if(reason.status === 400)
-      {
-        for(const key of Object.keys(reason.error)) {
-            if(key === 'oldPassword')
+        this.reporting.successMessage = "Password changed";
+      },
+      (reason: HttpErrorResponse) => {
+        if (reason.status === 400) {
+          for (const key of Object.keys(reason.error)) {
+            if (key === 'oldPassword')
               this.invalidPassword();
-        }
-      }
-      else
-        this.reporting.error = reason;
-    });
+          }
+        } else
+          this.reporting.error = reason;
+      });
   }
 
-  invalidPassword()
-  {
-      let oldPasswordCtl: AbstractControl = this.changePasswordForm.controls['oldPassword'];
-      let errors:{[key: string]: any} = {pattern: {badPassword:"Password Incorrect"}};
-      oldPasswordCtl.setErrors(errors);
-      oldPasswordCtl.markAsTouched({onlySelf: true}); //updateValueAndValidity({onlySelf: true, emitEvent: true});
+  invalidPassword() {
+    let oldPasswordCtl: AbstractControl = this.changePasswordForm.controls['oldPassword'];
+    let errors: { [key: string]: any } = {pattern: {badPassword: "Password Incorrect"}};
+    oldPasswordCtl.setErrors(errors);
+    oldPasswordCtl.markAsTouched({onlySelf: true}); //updateValueAndValidity({onlySelf: true, emitEvent: true});
   }
 
-  passwordValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      this.newPassword = control.value;
-      // Update the validation status of the confirmPassword field
-      if(this.confirmNewPassword !== "")
-      {
-        let cpControl:AbstractControl | null = this.changePasswordForm.get("confirmNewPassword");
-        cpControl?.updateValueAndValidity();
-      }
+  passwordValidator = (control: AbstractControl): ValidationErrors | null => {
+    this.newPassword = control.value;
+    // Update the validation status of the confirmPassword field
+    if (this.confirmNewPassword !== "") {
+      let cpControl: AbstractControl | null = this.changePasswordForm.get("confirmNewPassword");
+      cpControl?.updateValueAndValidity();
+    }
 
-      const ok = !new RegExp("^[A-Za-z0-9][A-Za-z0-9(){\[1*£$\\]}=@~?^]{7,31}$").test(control.value);
-      return ok ? {pattern: {value: control.value}} : null;
-    };
-  }
+    const ok = !new RegExp("^[A-Za-z0-9][A-Za-z0-9(){\[1*£$\\]}=@~?^]{7,31}$").test(control.value);
+    return ok ? {pattern: {value: control.value}} : null;
+  };
+
   /**
    * comparePasswords: Custom form field validator to check new password and confirm new password
    *                   are equal
@@ -74,10 +70,10 @@ export class ChangePasswordComponent implements OnInit{
    */
   comparePasswords = (control: AbstractControl): ValidationErrors | null => {
     this.confirmNewPassword = control.value;
-    let fg: FormGroup=control.parent as FormGroup;
+    let fg: FormGroup = control.parent as FormGroup;
     let ac: AbstractControl = fg?.controls['newPassword'];
     if (control.value == "" || control.value !== ac?.value) {
-      return {confirmNewPassword:  {value: control.value }};
+      return {confirmNewPassword: {value: control.value}};
     }
     return null;
   }
@@ -89,7 +85,7 @@ export class ChangePasswordComponent implements OnInit{
   ngOnInit(): void {
     this.changePasswordForm = new FormGroup({
       oldPassword: new FormControl(this.oldPassword, [Validators.required]),
-      newPassword: new FormControl(this.newPassword, [Validators.required, this.passwordValidator()]),
+      newPassword: new FormControl(this.newPassword, [Validators.required, this.passwordValidator]),
       confirmNewPassword: new FormControl(this.confirmNewPassword, [this.comparePasswords])
     }, {updateOn: "change"});
     this.changePasswordForm.markAllAsTouched();
