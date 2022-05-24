6a5684f247461403c5f6e31a47b9da160b1ae1a4
austinr <richard.david.austin@gmail.com>
2022-05-24 19:52:06 +0100

    Clean up the user interface a bit and make enable/disable work a bit more cleanly by waiting for 7 seconds after enabling Wi-Fi before getting the current Wi-Fi connection and other available ones. This give the Wi-Fi a chance to set itself up again before getting the current state.

--- cloudFrontEnd/src/app/wifi-settings/wifi-settings.component.ts
+++ cloudFrontEnd/src/app/wifi-settings/wifi-settings.component.ts
@@ -3,16 +3,23 @@ import {ReportingComponent} from '../reporting/reporting.component';
 import {WifiUtilsService} from '../shared/wifi-utils.service';
 import {CurrentWifiConnection} from '../shared/current-wifi-connection';
 import {MatCheckboxChange} from '@angular/material/checkbox';
+import {WifiDetails} from '../shared/BaseUrl/wifi-details';
+import {OnDestroy} from '@angular/core';
+import {MatSelect} from '@angular/material/select/select';
+import {timer} from 'rxjs';
 
 @Component({
   selector: 'app-wifi-settings',
   templateUrl: './wifi-settings.component.html',
   styleUrls: ['./wifi-settings.component.scss']
 })
-export class WifiSettingsComponent implements OnInit {
+export class WifiSettingsComponent implements OnInit, OnDestroy {
+  @ViewChild('selector') selector!: MatSelect;
   wifiEnabled: boolean = false;
-  currentWifiConnection!: CurrentWifiConnection;
+  currentWifiConnection: CurrentWifiConnection = new CurrentWifiConnection();
+  wifiList!: WifiDetails[];
   ethernetConnectionStatus!: string;
+  loading: boolean = true;
 
   @ViewChild(ReportingComponent) reporting!: ReportingComponent;
 
@@ -20,16 +27,45 @@ export class WifiSettingsComponent implements OnInit {
   }
 
   showWifi() {
+    this.loading = true;
     this.wifiUtilsService.getCurrentWifiConnection().subscribe((result) => {
       this.currentWifiConnection = result;
+      this.loading = false;
+      this.selector.value = this.currentWifiConnection.accessPoint;
     });
+  }
+
+  getLocalWifiDetails(): void {
+    if (this.wifiEnabled) {
+      this.wifiUtilsService.getLocalWifiDetails().subscribe((result) => {
+          this.wifiList = result
+            .filter(this.onlyUnique)
+            .sort((a, b) => parseInt(b.signal) - parseInt(a.signal));
+          this.showWifi();
+        },
+        reason => {
+          this.reporting.errorMessage = reason;
+        });
+    }
   }
 
   setWifiStatus($event: MatCheckboxChange) {
     let status: string = $event.checked ? 'on' : 'off';
+    this.loading = true;
 
     this.wifiUtilsService.setWifiStatus(status).subscribe((result) => {
         this.wifiEnabled = result.status === 'on';
+        if (this.wifiEnabled) {
+
+          // Allow time for the Wi-Fi connection to re-establish so iwconfig can detect it
+          timer(7000).subscribe(() => {
+            this.getLocalWifiDetails();
+            this.loading = false;
+          });
+        } else {
+          this.wifiList = [];
+          this.loading = false;
+        }
       },
       reason => {
         this.reporting.errorMessage = reason;
@@ -37,12 +73,21 @@ export class WifiSettingsComponent implements OnInit {
   }
 
   resetConnection() {
-     this.wifiUtilsService.restartCloudProxy().subscribe((result) => {
-       window.location.href = '/logoff';
-     },
-       reason => {
-         this.reporting.errorMessage = reason;
-       })
+    this.wifiUtilsService.restartCloudProxy().subscribe((result) => {
+        window.location.href = '/logoff';
+      },
+      reason => {
+        this.reporting.errorMessage = reason;
+      });
+  }
+
+  onlyUnique(value: WifiDetails, index: number, self: WifiDetails[]) {
+    let val: WifiDetails | undefined = self.find(a => a.ssid == value.ssid);
+    if (val !== undefined && val.ssid !== '') {
+      return self.indexOf(val) === index;
+    }
+
+    return false;
   }
 
   ngOnInit(): void {
@@ -54,12 +99,18 @@ export class WifiSettingsComponent implements OnInit {
       });
 
     this.wifiUtilsService.checkWifiStatus().subscribe((result) => {
-        this.showWifi();
+
         this.wifiEnabled = result.status === 'on';
+        if(this.wifiEnabled)
+          this.getLocalWifiDetails();
+        else
+          this.loading = false;
       },
       reason => {
         this.reporting.errorMessage = reason;
       });
   }
 
+  ngOnDestroy(): void {
+  }
 }
